e8e39549837f38fedc0c63f486e95026a7f22eac
Abdimahad hassan salat <mahadgaceyte1@gmail.com>
2024-04-16 11:42:49 +0300

    inializing code

--- solution1.js
+++ solution1.js
@@ -1,8 +1,13 @@
 function swapCASE(str){
+    return str.replace(/[a-zA-Z]/g, function (char) {
+        return char === char.toUpperCase() ? char.toLowerCase() : char.toUpperCase();
+    });
+}
+
     return str.replace(/[a-zA-Z]/g,)
     function (char) {
         return char ===char.toUppercase()
         char.tolowerCase() :char.toUppercase();
 
     }
-}
+
--- solution2.js
+++ solution2.js
@@ -1,7 +1,11 @@
 function generateArrays(start, end) {
+    const length = Math.abs(end - start )+1;
+    const direction = Math.sign(end - start);
+    return Array.from({length}, (_, index) => start + index * direction);
+}
+
     const length = math.abs(end - start )+1;
     const direction =math.sign(end-start);
     return Array.from({length}, (_,index)
     => start+index * direction);
     
-}
